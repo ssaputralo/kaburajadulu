03e985d5f3e2d5ecf5c948b03ee0f9765dd2636f
ssaputralo <pandi.work@outlook.com>
2025-04-26 20:26:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ with st.sidebar:
     st.button("Click me!")
 
 st.title("#KaburAjaDulu")
+st.write("Ini merupakan platform untuk membantu kamu dalam mempersiapkan diri dalam melanjutkan pendidikan, bekerja, atau menetap di luar negeri.")
+st.write("Kami menyediakan berbagai informasi dan sumber daya yang dapat membantu kamu dalam mencapai tujuan tersebut.")
+
+st.write("Kamu rencana mau ke negara mana nih?")
+st.selectbox("Pilih negara:", ["Jerman", "Belanda", "Swedia", "Finlandia", "Norwegia"])
